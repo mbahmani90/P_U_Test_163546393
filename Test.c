331cad1e49a170fc837c189e562aa641ae6f9e92
Majid <mjbahmanii@gmail.com>
2022-11-01 20:17:21 +0330

    Code cleaned

--- Test.c
+++ Test.c
@@ -2,58 +2,53 @@
 #include <stdio.h>
 #include "stdbool.h"
 
-// int A[12][5]         = {{0}, {0}, {0}, {0}, {0}, {2 , 4}, {0}, {0}, {0}, {0}, {5 , 3}, {0}};
-// int sizeItemA[12]    = { 0 ,  0 ,  0 ,  0 ,  0 ,  2     ,  0 ,  0 ,  0 ,  0 ,  2     ,  0 };
-// int isParent [12][5] = {{0}, {0}, {0}, {0}, {0}, {0 , 0}, {0}, {0}, {0}, {0}, {1 , 0}, {0}};
-// int B[12]            = {5 , 10};
-// int sizeA            = 11;
-// int sizeB            = 2;
-// int root             = 10;
+// int node[12][5]       = {{0}, {0}, {0}, {0}, {0}, {2 , 4}, {0}, {0}, {0}, {0}, {5 , 3}, {0}};
+// int sizeNodeItem[12]  = { 0 ,  0 ,  0 ,  0 ,  0 ,  2     ,  0 ,  0 ,  0 ,  0 ,  2     ,  0 };
+// int parent[12]        = {5 , 10};
+// int sizeNode          = 11;
+// int sizeParent        = 2;
+// int root              = 10;
+// int test_array[10][2] = {{4 , 3} , {4 , 2} , {3 , 2} , {2 , 3}};
+// int test_array_size   = 4;
 
-// int test_array[10][2]      = {{4 , 3} , {4 , 2} , {2 , 3}};
-// int test_array_size        = 3;
-
-int A[1002][1002]      = {{0}};
-int sizeItemA[1002]    = { 0 };
-int B[1002]            = { 0 };
-int sizeA            = 0;
-int sizeB            = 0;
-int root             = 0;
-
+int node[1002][1002]       = {{0}};
+int sizeNodeItem[1002]     = { 0 };
+int parent[1002]           = { 0 };
+int sizeNode               = 0;
+int sizeParent             = 0;
+int root                   = 0;
 int test_array[1000002][2] = {{}};
 int test_array_size        = 0;
 
-int route_array[10][10]    = {{0}};
-int route_array1[10][10]   = {{0}};
-int route_array2[10][10]   = {{0}};
+int route_array [1002][1002] = {{0}};
+int route_array1[1002][1002] = {{0}};
+int route_array2[1002][1002] = {{0}};
 
-int route_array_size[10]   = { 0 };
-int route_array_size1[10]  = { 0 };
-int route_array_size2[10]  = { 0 };
+int route_array_size [1002]  = { 0 };
+int route_array_size1[1002]  = { 0 };
+int route_array_size2[1002]  = { 0 };
 
-void scan_route(int current , int target , int route_array_index , int size, int *temp_route_array[10] , int *array_size);
-void scan_route1(int current , int target , int route_array_index , int size);
-void scan_route2(int current , int target , int route_array_index , int size);
+void scan_route(int current , int target , int route_array_index , int size, int (*temp_route)[1002] , int *temp_route_size);
 int  are_branches_of_same_node(int current, int target , int *new_current , int *new_target);
 
-void append_to_B(int value){
+void append_to_parent_array(int value){
     
-    if(sizeB == 0){
-        B[sizeB] = value;
-        sizeB++;
+    if(sizeParent == 0){
+        parent[sizeParent] = value;
+        sizeParent++;
     }else{
-        int is_in_B = false;
-        for(int j = 0 ; j < sizeB ; j++){
+        int is_in_parent = false;
+        for(int j = 0 ; j < sizeParent ; j++){
             
-            if(value == B[j]){
-                is_in_B = true;
+            if(value == parent[j]){
+                is_in_parent = true;
                 break;
             }
             
         }
-        if(!is_in_B){
-            B[sizeB] = value;
-            sizeB++;
+        if(!is_in_parent){
+            parent[sizeParent] = value;
+            sizeParent++;
         }
     }
     
@@ -62,7 +57,7 @@ void append_to_B(int value){
 void get_values(){
     
     int number_of_nodes = 0;
-    int input_array[1000][3];
+    int input_array[1002][3];
     
     printf("Please Enter The Number Of Nodes: "); 
     scanf("%d", &number_of_nodes);
@@ -73,10 +68,8 @@ void get_values(){
         return;
     }
     for(int i = 0 ; i < number_of_nodes ; i++){
-        
         printf("N%d: " , (i + 1)); 
         scanf("%d %d %d", &input_array[i][0] , &input_array[i][1] , &input_array[i][2]);    
-
     }
     
     for(int i = 0 ; i < number_of_nodes ; i++){
@@ -85,39 +78,39 @@ void get_values(){
             root = input_array[i][0];
         }else{
             int temp_index = input_array[i][1];
-            A[temp_index][sizeItemA[temp_index]] = input_array[i][0];
-            sizeItemA[temp_index]++;
+            node[temp_index][sizeNodeItem[temp_index]] = input_array[i][0];
+            sizeNodeItem[temp_index]++;
             
-            if( (temp_index + 1) > sizeA){
-                sizeA = temp_index + 1;
+            if( (temp_index + 1) > sizeNode){
+                sizeNode = temp_index + 1;
             }
         }
         
         if(input_array[i][2] == 1){
-            append_to_B(input_array[i][0]);
+            append_to_parent_array(input_array[i][0]);
         }
         
     }
     
-    printf("B={");
-    for(int i = 0 ; i < sizeB ; i++){
-        if(i < sizeB - 1){
-            printf("%d," , B[i]);    
+    printf("Parent = {");
+    for(int i = 0 ; i < sizeParent ; i++){
+        if(i < sizeParent - 1){
+            printf("%d," , parent[i]);    
         }else{
-            printf("%d" , B[i]); 
+            printf("%d" , parent[i]); 
         }
         
     } 
     printf("}\n");
     
-    printf("A={");
-    for(int i = 0 ; i < sizeA ; i++){
+    printf("Node={");
+    for(int i = 0 ; i < sizeNode ; i++){
         printf("{");
-        for(int j = 0 ; j < sizeItemA[i] ; j++){
-            if(j < sizeItemA[i] - 1){
-                printf("%d," , A[i][j]);    
+        for(int j = 0 ; j < sizeNodeItem[i] ; j++){
+            if(j < sizeNodeItem[i] - 1){
+                printf("%d," , node[i][j]);    
             }else{
-                printf("%d" , A[i][j]); 
+                printf("%d" , node[i][j]); 
             }
             
         }
@@ -126,17 +119,15 @@ void get_values(){
     }
     printf("}\n");
     
-    printf("root = %d\n" , root);
-    printf("sizeA = %d\n" , sizeA);
-    printf("sizeB = %d\n" , sizeB);
-    printf("sizeItemA={");
-    for(int i = 0 ; i < sizeA ; i++){
-        if(i < sizeA - 1){
-            printf("%d," , sizeItemA[i]);    
+    printf("Root = %d\nNode Size = %d\nParent Size = %d\n" , root , sizeNode, sizeParent);
+
+    printf("Node Item Size = {");
+    for(int i = 0 ; i < sizeNode ; i++){
+        if(i < sizeNode - 1){
+            printf("%d," , sizeNodeItem[i]);    
         }else{
-            printf("%d" , sizeItemA[i]); 
+            printf("%d" , sizeNodeItem[i]); 
         }
-        
     } 
     printf("}\n");  
     
@@ -146,7 +137,6 @@ void get_values(){
     for(int i = 0 ; i < test_array_size ; i++){
         printf("T%d: " , (i + 1)); 
         scanf("%d %d", &test_array[i][0] , &test_array[i][1]); 
-        
     }
     
 }
@@ -160,10 +150,10 @@ int main()
         
         route_array1[i][0] = test_array[i][0];
         route_array_size1[i] = 1;
-        scan_route1(route_array1[i][0] , root , i , route_array_size1[i]);
+        scan_route(route_array1[i][0] , root , i , route_array_size1[i], route_array1 , route_array_size1);
         route_array2[i][0] = test_array[i][1];
         route_array_size2[i] = 1;
-        scan_route2(route_array2[i][0] , root , i , route_array_size2[i]);
+        scan_route(route_array2[i][0] , root , i , route_array_size2[i], route_array2 , route_array_size2);
     } 
     
     // for(int i = 0; i < test_array_size ; i++){
@@ -203,8 +193,6 @@ int main()
             
         }
         
-        // printf("i1 = %d   i2 = %d \n" , index1 , index2);
-        
         for(int j = 0 ; j <= index1 ;j++ ){
             route_array[i][j] = route_array1[i][j];
             route_array_size[i]++;
@@ -229,38 +217,20 @@ int main()
 }
 
 
-void scan_route1(int current , int target , int route_array_index , int size){
+void scan_route(int current , int target , int route_array_index , int size, int (*temp_route)[1002] , int *temp_route_size){
     
     int new_current = 0;
     int new_target  = 0;
     int temp_size = size;
     int result = are_branches_of_same_node(current , target , &new_current , &new_target);
     if(result == 2){
-        route_array1[route_array_index][temp_size] = new_current;
+        temp_route[route_array_index][temp_size] = new_current;
         temp_size++;
-        route_array_size1[route_array_index] = temp_size;
+        temp_route_size[route_array_index] = temp_size;
     }else if(result == 1){
-        route_array1[route_array_index][temp_size] = new_current;
+        temp_route[route_array_index][temp_size] = new_current;
         temp_size++;
-        scan_route1(new_current , new_target , route_array_index , temp_size);       
-    }
-    
-}
-
-void scan_route2(int current , int target , int route_array_index , int size){
-    
-    int new_current = 0;
-    int new_target  = 0;
-    int temp_size = size;
-    int result = are_branches_of_same_node(current , target , &new_current , &new_target);
-    if(result == 2){
-        route_array2[route_array_index][temp_size] = new_current;
-        temp_size++;
-        route_array_size2[route_array_index] = temp_size;
-    }else if(result == 1){
-        route_array2[route_array_index][temp_size] = new_current;
-        temp_size++;
-        scan_route2(new_current , new_target , route_array_index , temp_size);       
+        scan_route(new_current , new_target , route_array_index , temp_size , temp_route , temp_route_size);       
     }
     
 }
@@ -269,13 +239,13 @@ int are_branches_of_same_node(int current, int target , int *new_current , int *
     
     int index = 0;
     
-    for(int i = 0 ; i < sizeB ; i++){
+    for(int i = 0 ; i < sizeParent ; i++){
         
-        for(int j = 0 ; j < sizeItemA[B[i]] ; j++){
+        for(int j = 0 ; j < sizeNodeItem[parent[i]] ; j++){
             
-            if(current == A[B[i]][j]){
+            if(current == node[parent[i]][j]){
                 
-                index = B[i];
+                index = parent[i];
                 break;
                 
             }
@@ -288,15 +258,6 @@ int are_branches_of_same_node(int current, int target , int *new_current , int *
         *new_current = index;
         *new_target = target;
         return 2;        
-    }
-    
-    for(int i = 0 ; i < sizeItemA[index] ; i++){
-        
-        if(target == A[index][i]){
-            *new_current = index;
-            *new_target = target;
-            return 2;
-        }
     }
     
     *new_current = index;
